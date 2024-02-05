e17192d84d3a0cb9648784fb52aae0f0c85ef747
Brian <bmccormack15@gmail.com>
2024-02-05 01:45:45 -0800

    added basic html functionality using templates

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=boatShare.py
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,26 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'adun'}
+    posts = [
+        {
+            'author': {'username': 'jane'},
+            'body': 'jane pets a cat!'
+        },
+        {
+            'author': {'username': 'michael'},
+            'body': 'michael the archangel'
+        },
+        {
+            'author': {'username': 'jane'},
+            'body': 'jane pets a cat!'
+        },
+        {
+            'author': {'username': 'michael'},
+            'body': 'michael the archangel'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
